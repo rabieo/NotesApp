adcd078dfddc918a61e956b0c2069e5c41be9d93
Youssof Fahmy <youssoffahmy@gmail.com>
2020-07-06 23:01:48 -0400

    Testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import './App.css'
 
-export default function App() {
+export default function App(){
 	return (
 		<div className="App">
 			
 			<div className="sideBar">
+				<div className="note">
+					<h1>Note 1</h1>
+					Test Note
+				</div>
+				
+				<div className="note">
+					<h1>Note 1</h1>
+					This is all trash I'll delete it later
+				</div>
 				
 			</div>
 			
-			<div className="main">
-				
+			<div className="mainContainer">
+				<div className="main">
+					Lol this looks like a 1995 website 
+				</div>
 			</div>
 			
 		</div>
