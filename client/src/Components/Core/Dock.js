95bb2e0f70b994533ff2ea33b3f6760e1d6eb120
Youssof Fahmy <youssoffahmy@gmail.com>
2020-07-09 20:39:54 -0400

    styling updates to dock and noteCard

--- client/src/Components/Core/Dock.js
+++ client/src/Components/Core/Dock.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faBars} from '@fortawesome/free-solid-svg-icons'
+import './Dock.css'
 
 export default function Dock(props) {
 	
 	
 	return (
 		<div className="dock">
-			<button onClick={() => {
+			<button className= 'menuButton' onClick={() => {
 				props.setshowList(!props.showList)
-			}}>+</button>
+			}}><FontAwesomeIcon icon={faBars}/></button>
 		</div>
 	)
 }
